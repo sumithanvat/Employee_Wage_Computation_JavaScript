0221ecb88a59d950b5c9e663876cbd35d937744a
Sumithanvat <skhanvat@gmail.com>
2023-06-11 23:38:40 +0530

    Added:Check Employee attendance

---
+++ UC1_CheckAttandenceEmployee.js
@@ -0,0 +1,8 @@
+console.log("Welcome to Employee Wage Computation Problem");
+
+let attandence = Math.floor(Math.random()*2);
+if (attandence === 1){
+    console.log("Employee is prasent");
+}else {
+    console.log("Employee is absent ")
+}
